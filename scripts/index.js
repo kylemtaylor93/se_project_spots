ea88800fbe319be2d78203bc999d214a795c21ea
kylemtaylor93 <bktaylor05@outlook.com>
2025-06-22 17:31:49 -0600

    Updted Java

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,33 @@
 const editProfileBtn = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
+const editProfileForm = editProfileModal.querySelector(".modal__form");
+const newPostForm = editProfileModal.querySelector(".card__form");
+const editProfileNameInput = editProfileModal.querySelector(
+  "#profile-name-input"
+);
+const editProfileDescriptionInput = editProfileModal.querySelector(
+  "#profile-description-input"
+);
+
+const cardNameInput = editProfileModal.querySelector("#card-image-input");
+const cardDescriptionInput = editProfileModal.querySelector(
+  "#card-caption-input"
+);
 
 const newPostBtn = document.querySelector(".profile__new-post-btn");
 console.log("New Post Button:", newPostBtn);
 const newPostModal = document.querySelector("#new-post-modal");
+
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
 
+const profileNameEl = document.querySelector(".profile__name");
+const profileDescriptionEl = document.querySelector(".profile__description");
+
 editProfileBtn.addEventListener("click", function () {
+  editProfileNameInput.value = profileNameEl.textContent;
+  editProfileDescriptionInput.value = profileDescriptionEl.textContent;
+
   editProfileModal.classList.add("modal_is-opened");
 });
 
@@ -22,3 +42,20 @@ newPostBtn.addEventListener("click", function () {
 newPostCloseBtn.addEventListener("click", function () {
   newPostModal.classList.remove("modal_is-opened");
 });
+
+function handleEditProfileSubmit(event) {
+  event.preventDefault();
+  profileNameEl.textContent = editProfileNameInput.value;
+  profileDescriptionEl.textContent = editProfileDescriptionInput.value;
+  editProfileModal.classList.remove("modal_is-opened");
+}
+
+function handleEditNewPostSubmit(event) {
+  event.preventDefault();
+  console.log("cardNameInput.value)");
+  console.log("cardcaptionInput.value");
+  newPostModal.classList.remove("modal_is-opened");
+}
+
+editProfileForm.addEventListener("submit", handleEditProfileSubmit);
+editNewPostForm.addEventListener("submit", handleEditNewPostSubmit);
